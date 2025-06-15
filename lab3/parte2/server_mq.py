d34da9b4b60e6f9a448c78a1eea5be9ea3afb865
fati-zahra <81538798+fati-zahra@users.noreply.github.com>
2025-06-15 22:31:29 +0100

    Update server_mq.py

--- lab3/parte2/server_mq.py
+++ lab3/parte2/server_mq.py
@@ -1,3 +1,4 @@
+# server_mq.py
 import pika
 import json
 import model_mongo as model
@@ -11,26 +12,49 @@ channel.queue_bind(exchange='twitter-exchange', queue='twitter-queue', routing_k
 def callback(ch, method, properties, body):
     print(f"Received: {body}")
     msg = json.loads(body)
-    if msg['type'] == 'addUser':
-        print('add user')
-        try:
-            model.addUser(msg['data'])
-        except Exception as exc:
-            print(exc)
-    elif msg['type'] == 'updateUser':
-        print('update user')
-        try:
-            model.updateUser(msg['token', msg['data']])
-        except Exception as exc:
-            print(exc)
-    elif msg['type'] == 'removeUser': pass
-    elif msg['type'] == 'follow': pass
-    elif msg['type'] == 'unfollow': pass
-    elif msg['type'] == 'addTweet': pass
-    elif msg['type'] == 'addRetweet': pass
-    elif msg['type'] == 'like': pass
-    elif msg['type'] == 'dislike': pass
-    else: pass
+
+    try:
+        if msg['type'] == 'addUser':
+            print('add user')
+            model.add_user(msg['data'])
+
+        elif msg['type'] == 'updateUser':
+            print('update user')
+            model.update_user(msg['token'], msg['data'])
+
+        elif msg['type'] == 'removeUser':
+            print('remove user')
+            model.remove_user(msg['token'])
+
+        elif msg['type'] == 'follow':
+            print('follow user')
+            model.follow(msg['token'], msg['data'])
+
+        elif msg['type'] == 'unfollow':
+            print('unfollow user')
+            model.unfollow(msg['token'], msg['data'])
+
+        elif msg['type'] == 'addTweet':
+            print('add tweet')
+            model.add_tweet(msg['token'], msg['data'])
+
+        elif msg['type'] == 'addRetweet':
+            print('add retweet')
+            model.add_retweet(msg['token'], msg['data'])
+
+        elif msg['type'] == 'like':
+            print('like tweet')
+            model.like(msg['token'], msg['data'])
+
+        elif msg['type'] == 'dislike':
+            print('dislike tweet')
+            model.dislike(msg['token'], msg['data'])
+
+        else:
+            print(f"Unknown message type: {msg['type']}")
+
+    except Exception as exc:
+        print(f"Error handling message: {exc}")
+
 channel.basic_consume(queue='twitter-queue', on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
-
